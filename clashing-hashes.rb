a0b3f0e554194d6adf3eb87b731e2d3a623260bb
MakerSquare Student <students@themakersquare.com>
2013-09-20 15:45:21 -0500

    so close

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,20 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'twitter'
+require_relative 'lib/follower_clash'
 
 get '/' do
-  'hello world'
+	# @name='Mike'
+ #  @show_description = false
+  erb :index
+end
+
+post '/twitter' do
+	@user1 = FollowerClash::User.new(params[:user1])
+	@user2 = FollowerClash::User.new(params[:user2])
+
+	@result = FollowerClash::Comparer.new(@user1, @user2).compare
+
+	erb :twitter
 end
